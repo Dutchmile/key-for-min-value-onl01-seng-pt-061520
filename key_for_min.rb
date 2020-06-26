b8ab3e722e2fa2d185b97bc3e3ec72e2d2654edb
Christopher Dutcher <cadutcher@icloud.com>
2020-06-26 01:21:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
 
   min_value = 0
-  min_name = ""
+  min_name = nil
   name_hash.each do |name, value|
     if
       min_value == 0 || value < min_value
